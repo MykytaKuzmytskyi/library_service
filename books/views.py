13ec1e42d058093d5e24bb943dc0ae4184af6e57
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-04-22 14:51:51 +0300

    Initialize users app

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -8,3 +9,13 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
