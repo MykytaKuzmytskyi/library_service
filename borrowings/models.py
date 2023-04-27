ccfcbff16c0cef7c67205357046720f631a03ac1
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-04-27 18:08:24 +0300

    Added __str__ to Borrowing model.

--- borrowings/models.py
+++ borrowings/models.py
@@ -5,10 +5,14 @@ from books.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateTimeField()
-    expected_return_date = models.DateTimeField()
-    actual_return_date = models.DateTimeField()
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    expected_return_date = models.DateTimeField(blank=True, null=True)
+    actual_return_date = models.DateTimeField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="books")
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return f"{self.book} for {self.user}" \
+               f" ({self.borrow_date.strftime('%Y.%m.%d %H:%M')})"
