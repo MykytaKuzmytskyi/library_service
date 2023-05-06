d5f334ff2a84c83ac5e8b398465da71799493a77
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-05-06 16:05:49 +0300

    Added @action for borrowing return.

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status, mixins
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -20,7 +21,7 @@ class BorrowingViewSet(
     GenericViewSet
 ):
     queryset = Borrowing.objects.all()
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -73,29 +74,28 @@ class BorrowingViewSet(
 
         if self.action == "create":
             return BorrowingCreateSerializer
+
+        if self.action == "return_borrowing":
+            return BorrowingReturnSerializer
+
         return BorrowingListSerializer
 
-
-class BorrowingReturnView(mixins.UpdateModelMixin, GenericViewSet):
-    queryset = Borrowing.objects.all()
-    permission_classes = (IsAdminUser,)
-    serializer_class = BorrowingReturnSerializer
-
-    def update(self, request, *args, **kwargs):
+    @action(
+        methods=["PUT"],
+        detail=True,
+        url_path="return",
+        permission_classes=[IsAdminUser]
+    )
+    def return_borrowing(self, request, *args, **kwargs) -> Response:
         partial = kwargs.pop("partial", False)
-        instance = self.get_object()
+        borrowing = self.get_object()
         serializer = self.get_serializer(
-            instance, data=request.data, partial=partial
+            borrowing, data=request.data, partial=partial
         )
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        if serializer.is_valid(raise_exception=True):
+            borrowing.book.inventory += 1
+            borrowing.book.save()
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-        if getattr(instance, "_prefetched_objects_cache", None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        instance.book.inventory += 1
-        instance.book.save()
-
-        return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
